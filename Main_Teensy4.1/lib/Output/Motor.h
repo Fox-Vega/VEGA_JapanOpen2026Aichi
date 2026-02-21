541467815cf2d51699cae0ce4115f5cd4f910787
Fox-VEGA <Fox.Vega.Soft@gmail.com>
2026-02-21 19:16:56 +0900

    Refactor Motor class method signatures and comments

--- Main_Teensy4.1/lib/Output/Motor.h
+++ Main_Teensy4.1/lib/Output/Motor.h
@@ -7,39 +7,46 @@
 class MyMOTOR {
     public:
         void setup(); //初期設定
-        void run(int movement_azimuth, int power_, int dir_azimuth); //（進行方向、出力、方向）
+        void run(int movement_azimuth_, int movement_power_, int difix_azimuth_); //（進行方向、出力、方向）
         void free(); //自由回転
         int difix(int target_azimuth); //姿勢制御用（微分先行型PD）
         int get_azimuth(); //現在の進行方向を取得
         int get_intensity(); //現在の出力を取得
         void lock(bool stat); //モーターの信号ロック　０＝無　１＝有
-        void stabilization(int stat); //姿勢制御の有無　０＝無　１＝有　２＝敵
+        void set_difix(int stat); //姿勢制御のモード　０＝無　１＝有　２＝敵
 
     private:
         //調整
-        const float kp = 2.5; //比例　感度を上げる
-        const float dkp = 2.0; //ゴールを向く（dzone外の比例）
-        const float dzkp = 1.5; //ゴールを向く（dzoneの比例（dzoneに微分無し））
+        const float kp = 2.5; //通常の比例
+        const float dkp = 2.0; //dzone外の比例
+        const float dzkp = 1.5; //dzone内の比例
         const float kd = 0.1; //微分　速度を抑える
-        const int dzone = 20; //dzone 範囲
-        const int pwmlimit = 200; //移動の出力リミット
-        const int difixlimit = 10; //姿勢制御の単体出力リミット
-        const int sharelimit = 150; //共有出力　姿勢制御単体出力リミットを超えた時に移動出力を削れる量
 
-        int max_power{};
+        const int dzone = 20; //dzoneの範囲（座標）
+        const int pwmlimit = 200; //移動の出力限界
+        const int difixlimit = 10; //姿勢制御の単体出力限界
+        const int sharelimit = 150; //姿勢制御の単体出力限界を超えた時に移動出力を削れる量
+        const int redzone = 210; //モーターの出力限界　保護用
 
+        int pwm{};
 
-        int azimuth;
-        int intensity;
+        //記録用
+        int rec_azimuth;
+        int rec_power;
+
+        //フラグ
         bool motor_lock = 0;
-        int motor_stabilization = 1;
+        int difix_mode = 0;
+
+        //出力データ
         float motor_power[4]{};
         float motor_power_[4]{};
-        float scale{};
 
+        //姿勢制御用の記録
         int prev_azimuth{};
         unsigned long lastupdate{};
 
+        //PIN情報
         const int motor_DPIN[4] = {4, 6, 10, 12};
         const int motor_PPIN[4] = {3, 5, 9, 11};
         const int motor_degrees[4] = {45, 135, 225, 315};
