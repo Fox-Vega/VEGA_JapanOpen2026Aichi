91aac0995644336eb3cdee7218061ff936fc9210
Fox-VEGA <Fox.Vega.Soft@gmail.com>
2026-02-21 19:51:46 +0900

    Fix message array size and update encoding logic

--- Line_XIAOESP32S3/lib/AIP/Communicate.cpp
+++ Line_XIAOESP32S3/lib/AIP/Communicate.cpp
@@ -4,22 +4,25 @@
 
 
 void Communicate::teensy_write(int ID) {
-    //ID1 = linedata
+    //start ID data1 data2 data3 key end
+    //ID １＝圧縮データ
 
-    uint8_t message1_write[6]{};
+    uint8_t message1_write[7]{};
+
     message1_write[0] = 195;
-    message1_write[5] = 231;
-    //start ID data data data end
+    message1_write[6] = 231;
 
     if (ID == 1) {
+        message1_write[1] = 1;
+
         for (int i = 2; i < 5; i++) {
             for (int j = 0; j < 8; j++) {
                 message1_write[i] |= (line.line_stat[(i-2) * 8 + j] & 0x01) << j;
             }
         }
 
-        message1_write[1] = 1;
     }
 
+    codec.encode(message1_write, 2, 4, 5);
     Serial1.write(message1_write, 6);
 }
