a7a9d1a9b776a56aebef43c35b5fefbd8f096e10
Fox-VEGA <Fox.Vega.Soft@gmail.com>
2026-02-21 19:16:37 +0900

    Refactor MyMOTOR methods for clarity and functionality

--- Main_Teensy4.1/lib/Output/Motor.cpp
+++ Main_Teensy4.1/lib/Output/Motor.cpp
@@ -5,7 +5,7 @@
 #include "Output.h"
 
 
-void MyMOTOR::setup() {
+void MyMOTOR::setup() { //初期設定
     for (int i = 0; i < 4; i++) {
         pinMode(motor_DPIN[i], OUTPUT);
         pinMode(motor_PPIN[i], OUTPUT);
@@ -14,55 +14,53 @@ void MyMOTOR::setup() {
     }
 }
 
-void MyMOTOR::run(int movement_azimuth, int power_, int dir_azimuth) {
-    power_ = constrain(power_, -pwmlimit, pwmlimit); //制限かける
+void MyMOTOR::run(int movement_azimuth_, int movement_power_, int difix_azimuth_) {
+    movement_power_ = constrain(movement_power_, -pwmlimit, pwmlimit); //移動出力の制限
 
     //記録
-    azimuth = movement_azimuth;
-    intensity = power_;
+    rec_azimuth = movement_azimuth_;
+    rec_power = movement_power_;
 
     //姿勢制御の出力を取得
     int difix = 0;
-    if (motor_stabilization) {
-        difix = mymotor.difix(dir_azimuth);
+    if (difix_mode) {
+        difix = mymotor.difix(difix_azimuth_);
     }
 
+    //仮出力の計算＆最高値の記録
     float peak = 0.0f;
     for (int i = 0; i < 4; i++) {
-        int azimuth_motor = (movement_azimuth - motor_degrees[i] + 360) % 360; //モーター軸をＹ軸にした時の進行方向
+        int motor_azimuth = (movement_azimuth_ - motor_degrees[i] + 360) % 360; //モーター軸をＹ軸とした時の進行方向
 
         //仮出力計算
-        myvector.get_cord(azimuth_motor, power_);
+        myvector.get_cord(motor_azimuth, movement_power_);
         motor_power_[i] = myvector.get_x();
 
-        if (fabs(motor_power_[i]) > peak) peak = fabs(motor_power_[i]); //最高値を記録
+        if (fabs(motor_power_[i]) > peak) peak = fabs(motor_power_[i]); //最高値の記録
     }
 
-    // int trim{};
+    //移動に使用できる最大出力の計算
     int trim = (abs(difix) - difixlimit);
-    if (trim > sharelimit) trim = sharelimit;
-    int mp = 210 - trim;
+    int max_power = movement_power_;
+    if (max_power > (redzone - trim)) max_power = redzone - trim;
 
-    max_power = power_;
-    if (power_ > mp) max_power = mp;
-
+    //出力の計算＆信号の送信
     for (int i = 0; i < 4; i++) {
-        // peak が 0 の場合は倍率 0 にして除算を避ける
-        scale = (peak == 0.0f) ? 0.0f : (fabs(motor_power_[i]) / peak); //倍率を計算
+        //除算防止
+        float scale = (peak == 0.0f) ? 0.0f : (fabs(motor_power_[i]) / peak); //倍率を計算
 
-        //出力計算（float）
+        //出力計算
         if (motor_power_[i] > 0) motor_power[i] = ((max_power - trim) * scale) + difix;
         else if (motor_power_[i] < 0) motor_power[i] = ((-max_power + trim) * scale) + difix;
         else motor_power[i] = difix;
 
-        motor_power[i] = constrain(motor_power[i], -210, 210);
+        //最終制限
+        motor_power[i] = constrain(motor_power[i], -redzone, redzone);
 
         //信号送信
         if (motor_lock == 0) {
-            float out = motor_power[i];
-            int pwm = (int)round(fabs(out));
-            if (pwm > pwmlimit) pwm = pwmlimit;
-            if (out >= 0) {
+            int pwm = (int)round(fabs(motor_power[i]));
+            if (motor_power[i] >= 0) {
                 analogWrite(motor_DPIN[i], pwm);
                 analogWrite(motor_PPIN[i], 0);
             } else {
@@ -81,40 +79,33 @@ void MyMOTOR::free() {
 }
 
 int MyMOTOR::difix(int target_azimuth) {
-    float dt = (millis() - lastupdate) / 1000.0; //秒
+    float dt = (millis() - lastupdate) / 1000.0; //秒に変換
 
+    //比例項
     float error = 0;
-    if (motor_stabilization == 2 && cam.get_ax(1) != 999) {
-        //角度差
+    if (difix_mode == 2 && cam.get_ax(1) != 999) {
         error = cam.get_x(1);
-        if (error > 180) {
-            error -= 360;
-        } else if (error < -180) {
-            error += 360;
-        }
+        if (error > 180) error -= 360;
+        else if (error < -180) error += 360;
     } else {
-        //角度差
         error = target_azimuth - gam.get_azimuth();
-        if (error > 180) {
-            error -= 360;
-        } else if (error < -180) {
-            error += 360;
-        }
+        if (error > 180) error -= 360;
+        else if (error < -180) error += 360;
     }
 
     //微分項
     int delta = gam.get_azimuth() - prev_azimuth;
-    if (delta > 180) {
-        delta -= 360;
-    } else if (delta < -180) {
-        delta += 360;
-    }
+    if (delta > 180) delta -= 360;
+    else if (delta < -180) delta += 360;
     float derivative = delta / dt;
 
-    int pwm = 0;
-    if (motor_stabilization == 2 && cam.get_x(1) != 999 && abs(cam.get_x(1)) > dzone) pwm = dkp * error - kd * derivative;
-    else if (motor_stabilization == 2 && cam.get_x(1) != 999) pwm = dzkp * error;
-    else pwm = kp * error - kd * derivative;
+    if (difix_mode == 2 && cam.get_x(1) != 999 && abs(cam.get_x(1)) > dzone) pwm = dkp * error; //deadzone外
+    else if (difix_mode == 2 && cam.get_x(1) != 999) pwm = dzkp * error; //deadzone内
+    else pwm = kp * error; //通常
+
+    pwm -= kd * derivative;
+
+    pwm = constrain(pwm, (difixlimit + sharelimit), -(difixlimit + sharelimit));
 
     lastupdate = millis();
     prev_azimuth = gam.get_azimuth();
@@ -123,17 +114,17 @@ int MyMOTOR::difix(int target_azimuth) {
 }
 
 int MyMOTOR::get_azimuth() {
-    return azimuth;
+    return rec_azimuth;
 }
 
 int MyMOTOR::get_intensity() {
-    return intensity;
+    return rec_power;
 }
 
 void MyMOTOR::lock(bool stat) {
     motor_lock = stat;
 }
 
-void MyMOTOR::stabilization(int stat) {
-    motor_stabilization = stat;
+void MyMOTOR::set_difix(int stat) {
+    difix_mode = stat;
 }
