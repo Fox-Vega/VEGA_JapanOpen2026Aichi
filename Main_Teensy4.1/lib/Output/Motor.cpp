53b3881579ae2520a89e43738b64e15780030bfd
Fox-VEGA <Fox.Vega.Soft@gmail.com>
2026-03-22 22:27:19 +0900

    Update Motor.cpp

--- Main_Teensy4.1/lib/Output/Motor.cpp
+++ Main_Teensy4.1/lib/Output/Motor.cpp
@@ -15,8 +15,6 @@ void MyMOTOR::setup() { //初期設定
 }
 
 void MyMOTOR::run(int movement_azimuth_, int movement_power_, int difix_azimuth_) {
-    movement_power_ = constrain(movement_power_, -pwmlimit, pwmlimit); //移動出力の制限
-
     //記録
     rec_azimuth = movement_azimuth_;
     rec_power = movement_power_;
@@ -31,8 +29,6 @@ void MyMOTOR::run(int movement_azimuth_, int movement_power_, int difix_azimuth_
     float peak = 0.0f;
     for (int i = 0; i < 4; i++) {
         int motor_azimuth = (movement_azimuth_ - motor_degrees[i] + 360) % 360; //モーター軸をＹ軸とした時の進行方向
-
-        //仮出力計算
         myvector.get_cord(motor_azimuth, movement_power_);
         motor_power_[i] = myvector.get_x();
 
@@ -40,19 +36,24 @@ void MyMOTOR::run(int movement_azimuth_, int movement_power_, int difix_azimuth_
     }
 
     //移動に使用できる最大出力の計算
-    int trim = (abs(difix) - difixlimit);
-    int max_power = movement_power_;
-    if (max_power > (redzone - trim)) max_power = redzone - trim;
+    movement_power_ = constrain(movement_power_, -pwmlimit, pwmlimit);
+    int max_power = redzone - abs(difix) + difixlimit;
+    if (movement_power_ < max_power) max_power = movement_power_;
 
-    //出力の計算＆信号の送信
     for (int i = 0; i < 4; i++) {
-        //除算防止
-        float scale = (peak == 0.0f) ? 0.0f : (fabs(motor_power_[i]) / peak); //倍率を計算
+        //倍率計算
+        float scale = (peak == 0.0f) ? 0.0f : (fabs(motor_power_[i]) / peak);
 
         //出力計算
-        if (motor_power_[i] > 0) motor_power[i] = ((max_power - trim) * scale) + difix;
-        else if (motor_power_[i] < 0) motor_power[i] = ((-max_power + trim) * scale) + difix;
-        else motor_power[i] = difix;
+        if (difix_mode != 2 || (difix_mode == 2 && ((difix <= 0 && (i == 0 || i == 1)) || (0 <= difix && (i == 2 || i == 3))))) {
+            if (motor_power_[i] > 0) motor_power[i] = (max_power * scale) + difix;
+            else if (motor_power_[i] < 0) motor_power[i] = (-max_power * scale) + difix;
+            else motor_power[i] = difix;
+        } else {
+            if (motor_power_[i] > 0) motor_power[i] = (max_power * scale);
+            else if (motor_power_[i] < 0) motor_power[i] = (-max_power * scale);
+            else motor_power[i] = 0;
+        }
 
         //最終制限
         motor_power[i] = constrain(motor_power[i], -redzone, redzone);
@@ -84,13 +85,24 @@ int MyMOTOR::difix(int target_azimuth) {
     //比例項
     float error = 0;
     if (difix_mode == 2 && cam.get_ax(1) != 999) {
+        error = cam.get_ax(1);
+        if (error > 180) error -= 360;
+        else if (error < -180) error += 360;
+        if (abs(cam.get_ax(1)) > dzone) pwm = dkp * error;
+        else pwm = dzkp * error;
+
+    } else if (difix_mode == 2 && cam.get_x(1) != 999) {
         error = cam.get_x(1);
         if (error > 180) error -= 360;
         else if (error < -180) error += 360;
+        if (abs(cam.get_x(1)) > dzone) pwm = dkp * error;
+        else pwm = dzkp * error;
+
     } else {
         error = target_azimuth - gam.get_azimuth();
         if (error > 180) error -= 360;
         else if (error < -180) error += 360;
+        pwm = kp * error;
     }
 
     //微分項
@@ -98,14 +110,9 @@ int MyMOTOR::difix(int target_azimuth) {
     if (delta > 180) delta -= 360;
     else if (delta < -180) delta += 360;
     float derivative = delta / dt;
-
-    if (difix_mode == 2 && cam.get_x(1) != 999 && abs(cam.get_x(1)) > dzone) pwm = dkp * error; //deadzone外
-    else if (difix_mode == 2 && cam.get_x(1) != 999) pwm = dzkp * error; //deadzone内
-    else pwm = kp * error; //通常
-
     pwm -= kd * derivative;
 
-    pwm = constrain(pwm, (difixlimit + sharelimit), -(difixlimit + sharelimit));
+    pwm = constrain(pwm, -(difixlimit + sharelimit), (difixlimit + sharelimit));
 
     lastupdate = millis();
     prev_azimuth = gam.get_azimuth();
